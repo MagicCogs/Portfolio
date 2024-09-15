cf3cf24984f39bfbda9043efe09adb6b070017d6
MagicCogs <thegingericing@gmail.com>
2024-09-15 18:01:44 +0200

    création du footer

---
+++ Portfolio/.history/src/views/FooterView_20240915180131.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>
+      <!-- Liens vers les réseaux sociaux -->
+      <div class="social-icons">
+        <a href="https://www.linkedin.com/feed/" target="_blank">
+          <img src="@/assets/img/linkedinicon.png" alt="LinkedIn" />
+        </a>
+  
+        <a href="https://github.com/MagicCogs" target="_blank">
+          <img src="@/assets/img/githubicon.png" alt="GitHub" />
+        </a>
+  
+        <a href="https://x.com/home" target="_blank">
+          <img src="@/assets/img/twittericon.png" alt="Twitter" />
+        </a>
+      </div>
+  
+      <!-- Affichage de la date de mise à jour -->
+      <div class="date">
+        <p>Date de dernière mise à jour: 10/09/2024</p>
+      </div>
+    </footer>
+  </template>
+  
